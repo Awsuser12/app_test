aaa49f7ca6b3e6ab7db1b299fd071a92bbb91242
Awsuser12 <harithsaman@gmail.com>
2024-12-18 11:02:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
+@app.route('/')
+def hello():
     return "Hello, Jenkins and Docker with Python 3.11!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    # Ensure the app is available externally by binding to 0.0.0.0
+    app.run(host='0.0.0.0', port=5000)
